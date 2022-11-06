c037ff3f4d0519fc1f6b823051e4f9bdf18c33ad
Sephley <hurley.joseph@protonmail.ch>
2022-11-06 13:52:07 +0100

    add gzip command

--- hydra.sh
+++ hydra.sh
@@ -10,21 +10,17 @@ echo -e "downloading reqired packages..."
 sudo apt install hydra
 sudo apt install zenity
 
+# download wordlists
 echo -e "$GREEN""downloading wordlists""$NC"
 wget -q https://github.com/Sephley/Hydra/blob/main/wordlists/rockyou.txt.gz
-#gzip -d rockyou.txt.gz 
+gzip -d wordlists/rockyou.txt.gz 
 wget -q https://github.com/Sephley/Hydra/blob/main/wordlists/10-million-password-list-top-1000000.txt
-
-echo -e "enter password to crack: "
-zenity --password
-read -r PASSWORD
-#echo -e "$PASSWORD" | sudo tee -a ~/
 
 echo -e "enter username: "
 read -r USERNAME
 
 echo -e "$GREEN""select the wordlist you would like to use\n""$NC"
-select d in */; do test -n "$d" && break; echo -e "$RED>>> Invalid Selection""$NC"; done
+select d in */wordlists/; do test -n "$d" && break; echo -e "$RED>>> Invalid Selection""$NC"; done
 cd "$d" && pwd
 
 hydra -l "$USERNAME" -P ""
